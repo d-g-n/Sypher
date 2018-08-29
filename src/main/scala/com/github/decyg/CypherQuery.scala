9873ce6d68ee720973e5821ed729228d69bdc39f
Declan Neilson <declan.g.neilson@gmail.com>
2018-08-29 01:41:13 +0100

    Experimentation

---
+++ src/main/scala/com/github/decyg/CypherQuery.scala
@@ -0,0 +1,87 @@
+package com.github.decyg
+
+trait CypherQuery {
+  val asCypher: String
+}
+
+// this is the root class?
+case class QueryStart(){
+
+  def create(cypherQuery: CypherQuery) = {
+    cypherQuery.asCypher
+  }
+
+  def MATCH = { MatchNode() }
+}
+
+trait ReadingClause extends CypherQuery{
+  def RETURN = {
+    new ReturnNode()
+  }
+}
+
+case class ReturnNode() extends CypherQuery {
+  override val asCypher: String = {
+    "RETURN"
+  }
+}
+
+case class MatchNode() extends ReadingClause{
+  override val asCypher: String = _
+}
+
+case class UnwindNode() extends ReadingClause{
+  override val asCypher: String = _
+}
+
+case class InQueryCallNode() extends ReadingClause{
+  override val asCypher: String = _
+}
+
+
+trait UpdatingClause extends CypherQuery
+
+case class CreateNode() extends UpdatingClause{
+  override val asCypher: String = _
+}
+
+case class MergeNode() extends UpdatingClause{
+  override val asCypher: String = _
+}
+
+case class DeleteNode() extends UpdatingClause{
+  override val asCypher: String = _
+}
+
+case class SetNode() extends UpdatingClause{
+  override val asCypher: String = _
+}
+
+case class RemoveNode() extends UpdatingClause{
+  override val asCypher: String = _
+}
+
+trait RegularQuery extends CypherQuery
+
+trait StandaloneCall extends CypherQuery
+
+trait SingleQuery extends CypherQuery{
+  def UNION() = {
+    this.asCypher + " UNION"
+  }
+}
+
+trait SinglePartQuery extends CypherQuery with SingleQuery
+
+trait MultiPartQuery extends CypherQuery with SingleQuery
+
+object yeet{
+  println(
+
+    QueryStart()
+      MATCH
+
+
+
+  )
+}
